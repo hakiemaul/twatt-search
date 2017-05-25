0298caa0d13e437af5364525f12c3fe25b29c2b7
hakiemaul <noersedya@gmail.com>
2017-05-25 10:53:52 +0700

    helpers done

--- helpers/util.js
+++ helpers/util.js
@@ -2,7 +2,11 @@ var getContent = function(data) {
   let result = [];
   data = JSON.parse(data)
   data.statuses.forEach(status => {
-    result.push(status.text)
+    let datas = {}
+    datas.username = status.user.screen_name;
+    datas.text = status.text;
+    datas.created_at = status.created_at
+    result.push(datas)
   })
   return result;
 }
